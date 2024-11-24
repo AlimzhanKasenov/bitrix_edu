1ce0031311f17248151c56a265391844ac87388e
Alimzhan Kasenov <alimzhan.kassenov@alsi.com>
2024-11-24 14:21:11 +0500

    Написать и подключить собственный класс системного логгера, который будет переопределять форматирование строк лога - добавлять слово OTUS в каждую строку.

---
+++ local/php_interface/init.php
@@ -0,0 +1,6 @@
+<?php
+use Bitrix\Main\Loader;
+
+Loader::registerAutoLoadClasses(null, [
+    'local\file\otus_file_exception_handler_log\OtusFileExceptionHandlerLog' => '/local/file/otus_file_exception_handler_log.php',
+]);
