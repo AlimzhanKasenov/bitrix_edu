4e6aebd9831e123acfdc5882c42b010acd2478a6
Alimzhan Kasenov <alimzhan.kassenov@alsi.com>
2024-12-10 15:53:15 +0500

    Модуль данных для таблицы БД

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,6 +1,9 @@
 <?php
 use Bitrix\Main\Loader;
 
+// Автозагрузка классов
 Loader::registerAutoLoadClasses(null, [
+    'Bitrix\Table\TableTable' => '/local/app/Models/customTable.php',
+
     'local\file\otus_file_exception_handler_log\OtusFileExceptionHandlerLog' => '/local/file/otus_file_exception_handler_log.php',
 ]);
