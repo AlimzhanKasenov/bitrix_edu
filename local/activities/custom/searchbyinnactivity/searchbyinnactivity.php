e477f1d442437a036a897c5f8720cd92269bb5cf
Alimzhan Kasenov <alimzhan.kassenov@alsi.com>
2025-02-03 11:00:51 +0500

    Написание своих активити для БП // ДЗ

--- local/activities/custom/searchbyinnactivity/searchbyinnactivity.php
+++ local/activities/custom/searchbyinnactivity/searchbyinnactivity.php
@@ -6,6 +6,7 @@ use Bitrix\Bizproc\FieldType;
 use Bitrix\Main\ErrorCollection;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Bizproc\Activity\PropertiesDialog;
+use Bitrix\Main\Loader;
 
 class CBPSearchByInnActivity extends BaseActivity
 {
@@ -15,12 +16,15 @@ class CBPSearchByInnActivity extends BaseActivity
 
         $this->arProperties = [
             'Inn'  => '',   // Входное значение (ИНН)
-            'Text' => null, // Выходной результат
+            'Text' => null, // Выходной результат (название компании)
+            // Новое свойство для ID созданного элемента
+            'ZakazchikElementID' => null,
         ];
 
-        // Указываем тип поля, чтобы движок понимал, что это строка
+        // Указываем типы возвращаемых свойств
         $this->SetPropertiesTypes([
             'Text' => ['Type' => FieldType::STRING],
+            'ZakazchikElementID' => ['Type' => FieldType::INT],
         ]);
     }
 
@@ -36,10 +40,17 @@ class CBPSearchByInnActivity extends BaseActivity
     {
         $errors = parent::internalExecute();
 
+        // Загружаем модуль iblock (для CIBlockElement)
+        if (!Loader::includeModule('iblock'))
+        {
+            $errors->setError(new \Bitrix\Main\Error('Модуль iblock не установлен'));
+            return $errors;
+        }
+
         // Пример использования Dadata
         $token  = "ТОКЕН";
         $secret = "СЕКРЕТ";
-        // Предполагаем, что класс Dadata загружен где-то до этого или autoload
+        // Предполагаем, что класс Dadata загружен через автозагрузчик
         $dadata = new Dadata($token, $secret);
         $dadata->init();
 
@@ -47,15 +58,45 @@ class CBPSearchByInnActivity extends BaseActivity
         $response = $dadata->suggest("party", $fields);
 
         $companyName = 'Компания не найдена!';
-        if (!empty($response['suggestions'])) {
-            $companyName = $response['suggestions'][0]['value'];
+        $elemId = 0;
+
+        if (!empty($response['suggestions']))
+        {
+            // Берём первую подсказку
+            $firstSuggest = $response['suggestions'][0];
+            $companyName  = $firstSuggest['value']; // Название организации
+            $data         = $firstSuggest['data'];  // Массив с остальными полями
+
+            // Создаём элемент в инфоблоке №27
+            $el = new \CIBlockElement();
+
+            $arLoad = [
+                "IBLOCK_ID" => 27,            // Ваш инфоблок для "Заказчика"
+                "NAME"      => $companyName,  // Поле NAME (обязательное)
+                "ACTIVE"    => "Y",
+                "PROPERTY_VALUES" => [
+                    "INN"  => $data["inn"]  ?? "",
+                    "KPP"  => $data["kpp"]  ?? "",
+                    "OGRN" => $data["ogrn"] ?? "",
+                    // Адрес чаще всего лежит здесь
+                    "ADRES" => $data["address"]["unrestricted_value"] ?? "",
+                ],
+            ];
+
+            $elemId = $el->Add($arLoad);
+            if (!$elemId)
+            {
+                // Если элемент не создался, запишем ошибку
+                $errors->setError(new \Bitrix\Main\Error("Не удалось создать элемент: ".$el->LAST_ERROR));
+            }
         }
 
-        // Записываем результат (станет доступен в "RETURN" => "Text")
-        $this->preparedProperties['Text'] = $companyName;
+        // Записываем результат в поля активности
+        $this->preparedProperties['Text']                = $companyName;
+        $this->preparedProperties['ZakazchikElementID']  = $elemId;
 
-        // Лог для отладки (не обязателен)
-        $this->log("Найдено: " . $companyName);
+        // Лог для отладки
+        $this->log("Найдено: " . $companyName . ", ID элемента = " . $elemId);
 
         return $errors;
     }
